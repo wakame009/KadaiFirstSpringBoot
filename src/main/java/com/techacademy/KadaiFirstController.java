0af25bf850f8d936bbe4514a7f8ac39e6a42914c
wakame009 <sayuri@sayurinombp.elecom>
2023-12-17 19:35:43 +0900

    KadaiFirstSpringBoot

--- src/main/java/com/techacademy/KadaiFirstController.java
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -1,10 +1,12 @@
 package com.techacademy;
 
-//import java.util.Calendar;
-//import java.util.Date;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,9 +16,42 @@ public class KadaiFirstController {
     public String index() {
         return "Hello SpringBoot!";
     }
-    @GetMapping("/dayofweek/{date}")
-    public String dispDayOfWeek(@PathVariable String date) {
-     // 現在の日付を取得
+    @GetMapping("now")
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String usrDate = br.readLine(); // ユーザ入力日付
+        br.close(); // リーダを閉じる
+
+        Calendar cal = Calendar.getInstance(); // 現在日時
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyymmdd"); 
+        Date date = sdf.parse(usrDate); // ユーザ入力日付解析
+        cal.setTime(date); // ユーザが入力した日付に更新
+
+        int weekNum = cal.get(java.util.Calendar.DAY_OF_WEEK); // 曜日
+        switch(weekNum) {
+        case(Calendar.SUNDAY) : 
+        case(Calendar.MONDAY) : 
+        case(Calendar.TUESDAY) : 
+        case(Calendar.WEDNESDAY) : 
+        case(Calendar.THURSDAY) : 
+        case(Calendar.FRIDAY) : 
+        case(Calendar.SATURDAY) : 
+        }
+
+        sdf.applyPattern("yyyymmdd"); // フォーマッタのパターンを変更
+        }
+
+        static void dispDayOfWeek(String dateStr,String dayOfWeek) {
+        String now = null;
+        System.out.println( dayOfWeek + now);
+        
+        
+        //public String dispDayOfWeek() {
+        //String now = Calendar.now().format( Date.ofPattern("yyyymmdd") );
+        //return "実行結果：" + now;
+        
+        //public String dispDayOfWeek(@PathVariable String date) {
+        // 現在の日付を取得
         //Date date = new Date();
 
         // Calendarインスタンスを取得
@@ -35,9 +70,17 @@ public class KadaiFirstController {
         
         //String dayOfWeek = Calendar.dayOfWeek().format( Date.ofPattern("yyyymmdd") );
 
-        return "Tuesday";  
+        //return "Tuesday";  
     }
         
+    private Object Calendar(String string) {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+    private Object Date() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
     //足し算
     //@GetMapping("calcPlus")
     public void calcPlus() {
