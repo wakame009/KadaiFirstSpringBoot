601e1d6683261f9b9778f58203165fb1cb14308a
wakame009 <sayuri@sayurinombp.elecom>
2023-12-17 22:58:45 +0900

    KadaiFirstSpringBoot

--- src/main/java/com/techacademy/KadaiFirstController.java
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -52,7 +52,7 @@ public class KadaiFirstController {
         return "計算結果：" + res;
     }
     
-    @GetMapping("/devide/{val1}/{val2}")
+    @GetMapping("/divide/{val1}/{val2}")
     public String calcDivide(@PathVariable int val1, @PathVariable int val2) {
         int res = 0;
         res = val1 / val2;
